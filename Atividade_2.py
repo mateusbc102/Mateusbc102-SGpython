243b544b1c907600fc18682f164c2c1cd6bc486e
mateusbc102 <mateusbc102@gmail.com>
2025-10-03 19:05:07 -0300

    Create Atividade_2.py
    Criacao da atividade 2 realizada no curso

---
+++ Atividade_2.py
@@ -0,0 +1,9 @@
+idade =  27
+age = "minha idade  "
+#print  (age  + str (idade))
+altura = "1.70"
+idade += float (altura)
+#print ("e minha altura " + altura + " somada a minha idade resulta em " + str (idade ) )
+altura_correta = True
+idade_correta = False
+print ("minha altura esta " + str (altura_correta) + " e minha idade esta " + str (idade_correta))
