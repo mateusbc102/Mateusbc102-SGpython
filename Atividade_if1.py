ccebb142b12653dbcb2cdaeb08a073d5531674b8
mateusbc102 <mateusbc102@gmail.com>
2025-10-03 19:55:45 -0300

    Create Atividade_if1.py
    Primeira atividade de IF , curso SG Python

---
+++ Atividade_if1.py
@@ -0,0 +1,11 @@
+chuva = input('Está chovendo? responda com: "s" ou "n" ')
+if chuva == "s":
+  chuva = True
+  print ("Está chovendo")
+if chuva == "n":
+  chuva = False
+  print ("Não está chovendo")
+if chuva ==  True:
+  print ("Levar guarda-chuva")
+if chuva == False:
+  print ("Não levar guarda-chuva")
