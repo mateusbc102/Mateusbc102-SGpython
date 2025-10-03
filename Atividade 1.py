5a666241010544b728a711279dface33d806e4b2
mateusbc102 <mateusbc102@gmail.com>
2025-10-03 19:04:05 -0300

    Create Atividade 1.py
    Criacao de arquivo com a primeira atividade realizada no curso

---
+++ Atividade 1.py
@@ -0,0 +1,10 @@
+camelCase = "eu sei fazer print assim"
+happyCase = "e assim:"
+print camelCase + " hello World!"
+print (happyCase + " hello World!")
+contCamel = "24"
+contHappy = "8"
+print ("camelCase tem " + contCamel + " happyCase tem " + contHappy)
+modulo = 24 % 8
+print "e o modulo da quantidade de caracteres é"
+print (modulo)
