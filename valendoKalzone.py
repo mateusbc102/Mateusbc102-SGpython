44be7eec0a08bc4ed26fc8b69b44573573f1c0c0
mateusbc102 <mateusbc102@gmail.com>
2025-10-17 19:24:18 -0300

    Update valendoKalzone.py
    adicionado funcao or

--- valendoKalzone.py
+++ valendoKalzone.py
@@ -1,25 +1,59 @@
 trabalhoEstressante = input ('Seu trabalho é estressante? (Responda com "s" ou "n")')
-tempoDeTrabalho = input ('Quantas horas de trabalho diario? ')
-FazAtividadeFisica = input ('Faz atividade fisica semanalmente?(Responda com "s" ou "n")')
-alimentacaoRuim = input ('Sua alimentação é inadequada? (Responda com "s" ou "n")')
-if trabalhoEstressante == 's':
-  if int(tempoDeTrabalho) > 6:
-    if FazAtividadeFisica == 'n':
-      if alimentacaoRuim == 's':
-        print ('cuidado!')
+if trabalhoEstressante == 's' or  trabalhoEstressante == 'n':
+  tempoDeTrabalho = float( input ('Quantas horas de trabalho diario? '))
+  fazAtividadeFisica = input ('Faz atividade fisica semanalmente?(Responda com "s" ou "n")')
+  if fazAtividadeFisica == 's' or  fazAtividadeFisica == 'n':
+    alimentacaoRuim = input ('Sua alimentação é inadequada? (Responda com "s" ou "n")')
+    if alimentacaoRuim == 's' or  alimentacaoRuim == 'n':
+      if trabalhoEstressante == 's':
+        if (tempoDeTrabalho) > 6:
+          if fazAtividadeFisica == 'n':
+            if alimentacaoRuim == 's':
+              print ('cuidado!')
+            else:
+              print ('Pelo menos come bem!')
+          else:
+            if alimentacaoRuim == 's':
+              print ('Apenas 1 de 4!')
+            else:
+              print ('Na hora de pedir demissão!')
+        else:
+          if fazAtividadeFisica == 'n':
+            if alimentacaoRuim == 's':
+              print ('atenção!')
+            else:
+              print ('2 de 4, nada mal!')
+          else:
+            if alimentacaoRuim == 's':
+              print ('No meio do caminho!')
+            else:
+              print ('Uau! 3 de 4!')
       else:
-        print ('Pelo menos come bem!')
+        if int(tempoDeTrabalho) > 6:
+          if fazAtividadeFisica == 'n':
+            if alimentacaoRuim == 's':
+              print ('atenção!')
+            else:
+              print ('Não está ruim, mas pode melhorar!')
+          else:
+            if alimentacaoRuim == 's':
+              print ('Quase lá! Melhora essa dieta!')
+            else:
+              print ('Quase fechou!!!')
+        else:
+          if fazAtividadeFisica == 'n':
+            if alimentacaoRuim == 's':
+              print ('Para melhorar só depende de você!')
+            else:
+              print ('É isso aí! Só falta se mexer!')
+          else:
+            if alimentacaoRuim == 's':
+              print ('Já está chegando! Agora é só fechar a boca!')
+            else:
+              print ('Gostei de ver! Gabaritou!!!')
     else:
-      if alimentacaoRuim == 's':
-        print ('no meio do caminho!')
-      else:
-        print ('Na hora de pedir demissão!')
+      print ('Escreveu errado.')
   else:
-    if FazAtividadeFisica == 'n':
-      if alimentacaoRuim == 's':
-        print ('atenção!')
+    print ('Escreveu errado.')
 else:
-  if int(tempoDeTrabalho) > 6:
-    if FazAtividadeFisica == 'n':
-      if alimentacaoRuim == 's':
-        print ('atenção!')
+  print ('Escreveu errado.')
