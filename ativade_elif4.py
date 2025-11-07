17ddc49bf359d9321bfbec2e816816dd3d0a2b60
mateusbc102 <mateusbc102@gmail.com>
2025-11-07 19:14:53 -0300

    Update ativade_elif4.py
    inclusao de  (s/n)

--- ativade_elif4.py
+++ ativade_elif4.py
@@ -1,8 +1,8 @@
 idade = float(input('Qual a sua idade? '))
-experiencia = input('Tem experiencia? ')
-antecedentes_criminais = input('possui antecedentes criminais? ')
-ensino_superior = input('Possui ensino superior? ')
-indicaçao = input('Foi indicado por alguem? ')
+experiencia = input('Tem experiencia? (s/n) ')
+antecedentes_criminais = input('possui antecedentes criminais? (s/n) ')
+ensino_superior = input('Possui ensino superior? (s/n) ')
+indicaçao = input('Foi indicado por alguem? (s/n) ')
 if antecedentes_criminais == 's':
     print('Infelizmente nao temos vagas disponiveis.')
 elif idade >= 18 and (experiencia == 's' or (ensino_superior == 's' or indicaçao =='s')):
