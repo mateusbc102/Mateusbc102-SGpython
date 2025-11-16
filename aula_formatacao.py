a8c8656c1b4ec48d02fb2f608360fd6e9edc39bc
mateusbc102 <mateusbc102@gmail.com>
2025-11-15 21:55:20 -0300

    Create aula_formatacao.py
    Aula de formatação avulsa.

---
+++ aula_formatacao.py
@@ -0,0 +1,5 @@
+string_1 = "camelot"
+string_2 = 50
+print ("vamos para a casa da {}. Ela nos convidou para chegar mais {}.".format(string_1,string_2))
+print (f"vamos para a casa da {string_1}. Ela nos convidou para chegar mais {string_2}.")
+print ("vamos para a casa da %s. Ela nos convidou para chegar mais %s." % (string_1,string_2))
