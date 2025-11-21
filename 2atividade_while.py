2840621c1f4810636a6cb294432cd5027a92d44d
mateusbc102 <mateusbc102@gmail.com>
2025-11-21 20:50:47 -0300

    Create 2atividade_while.py
    Segunda atividade com while e  uso da biblioteca random.

---
+++ 2atividade_while.py
@@ -0,0 +1,27 @@
+import random
+
+print ("Vamos Jogar?")
+chances = int(input("Escolha quantas chances deseja ter para acertar o número secreto: "))
+maximo = int(input("Escolha o intervalo em que o número secreto deve estar: "))
+
+numero_secreto = random.randint(1,maximo)
+numero_escolhido = 0
+
+while numero_secreto != numero_escolhido and chances > 0:
+
+    numero_escolhido = int(input(f"Escolha um numero para adivinhar (Escolha de 1 a {maximo}): "))
+    while numero_escolhido < 1 or numero_escolhido > maximo:
+        print ("Numero escolhido fora do intervalo.")
+        numero_escolhido = int(input(f"Escolha um numero para adivinhar (Escolha de 1 a {maximo}): "))
+        
+    chances -= 1
+
+    if numero_secreto == numero_escolhido:
+        print (f"Acertou! O numero era {numero_secreto}. E ainda sobraram {chances} chances.")
+    elif numero_secreto < numero_escolhido:
+        print (f"Errou, o numero secreto é menor do que esse. Voce possui {chances} chances.")
+    else:
+        print(f"Errou, o numero secreto é maior do que esse. Voce possui {chances} chances.")
+    
+if chances == 0 and numero_secreto != numero_escolhido:
+    print (f"Voce perdeu todas as chances, o número era {numero_secreto}.")
