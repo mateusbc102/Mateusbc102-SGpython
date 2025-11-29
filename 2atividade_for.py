732358f292d127259ec0ebd07ac90b4e377a4e48
mateusbc102 <mateusbc102@gmail.com>
2025-11-28 21:32:01 -0300

    Create 2atividade_for.py
    segunda atividade de for.

---
+++ 2atividade_for.py
@@ -0,0 +1,8 @@
+jogos = ["need for speed","fruit ninja","minecraft", "roblox"]
+for jogo in jogos:
+    if jogo == "need for speed":
+      print("amo esse jogo: " + jogo)
+    elif jogo == "fruit ninja":
+      print ("odeio esse jogo " + jogo)
+    else:
+      print("Esse jogo é comum  " + jogo)
