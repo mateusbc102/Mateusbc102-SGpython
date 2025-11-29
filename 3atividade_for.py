b9478c679f7b632c696604f733440d76a26f8c13
mateusbc102 <mateusbc102@gmail.com>
2025-11-28 21:52:40 -0300

    Create 3atividade_for.py
    terceira atividade for.

---
+++ 3atividade_for.py
@@ -0,0 +1,16 @@
+nome_usuario = input("Qual o seu nome?: ")
+for letra in nome_usuario:
+  if letra == "m":
+    print (f"a letra {letra} pertence ao meu nome")
+  elif letra == "a":
+    print (f"a letra {letra} pertence ao meu nome")
+  elif letra == "t":
+    print (f"a letra {letra} pertence ao meu nome")
+  elif letra == "e":
+    print (f"a letra {letra} pertence ao meu nome")
+  elif letra == "u":
+    print (f"a letra {letra} pertence ao meu nome")
+  elif letra == "s":
+    print (f"a letra {letra} pertence ao meu nome")
+  else:
+    print (f" a Letra {letra} nao percente ao meu nome")
